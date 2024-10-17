ba779c716cfa986be772533dfec42ccd283d674f
Giulianogoliver <giulianogoliver84@gmail.com>
2024-10-16 21:14:50 -0300

    change mockito version and unit tests for Controller

--- src/test/java/br/ce/wcaquino/taskbackend/controller/TaskControllerTest.java
+++ src/test/java/br/ce/wcaquino/taskbackend/controller/TaskControllerTest.java
@@ -1,26 +1,75 @@
 package br.ce.wcaquino.taskbackend.controller;
 
+import java.time.LocalDate;
+
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import br.ce.wcaquino.taskbackend.model.Task;
+import br.ce.wcaquino.taskbackend.repo.TaskRepo;
+import br.ce.wcaquino.taskbackend.utils.ValidationException;
 
 public class TaskControllerTest {
 
-    @Test
-    public void naoDeveSalvarTarefaSemDescricao(){
+    @Mock
+    private TaskRepo todoRepo;
 
+    @InjectMocks
+    private TaskController controller;
+
+    @Before
+    public void setup(){
+        MockitoAnnotations.openMocks(this);
     }
 
     @Test
-    public void naoDeveTarefaSemData(){
-
+    public void naoDeveSalvarTarefaSemDescricao() {
+        Task todo = new Task();
+        todo.setDueDate(LocalDate.now());
+        try {
+            controller.save(todo);
+            Assert.fail("O teste falhou!");
+        } catch (ValidationException e) {
+            Assert.assertEquals("Fill the task description", e.getMessage());
+        }
     }
 
     @Test
-    public void naoDeveSalvarTarefaComDataPassada(){
-
+    public void naoDeveTarefaSemData() {
+        Task todo = new Task();
+        todo.setTask("Tarefa Teste");
+        try {
+            controller.save(todo);
+            Assert.fail("O teste falhou!");
+        } catch (ValidationException e) {
+            Assert.assertEquals("Fill the due date", e.getMessage());
+        }
     }
 
     @Test
-    public void deveSalvarTerefaComSucesso(){
-        
+    public void naoDeveSalvarTarefaComDataPassada() {
+        Task todo = new Task();
+        todo.setDueDate(LocalDate.of(2010, 1, 1));
+        todo.setTask("Tarefa Teste");
+        try {
+            controller.save(todo);
+            Assert.fail("O teste falhou!");
+        } catch (ValidationException e) {
+            Assert.assertEquals("Due date must not be in past", e.getMessage());
+        }
+    }
+
+    @Test
+    public void deveSalvarTerefaComSucesso() throws ValidationException {
+        Task todo = new Task();
+        todo.setDueDate(LocalDate.now());
+        todo.setTask("Tarefa Teste");
+        controller.save(todo);
+        Mockito.verify(todoRepo).save(todo);
     }
 }
