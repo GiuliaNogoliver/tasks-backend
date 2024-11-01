48507af15ea85b120c97d1cafc2649fac9daf65a
Giulianogoliver <giulianogoliver84@gmail.com>
2024-11-01 17:12:07 -0300

    first commit

---
+++ Jenkinsfile
@@ -0,0 +1,70 @@
+pipeline {
+    agent any
+    stages {
+        stage('Build Backend') {
+            steps {
+                bat 'mvn clean package -DskipTests=true'
+            }
+        }
+
+        stage('Unit Tests') {
+            steps {
+                bat 'mvn test'
+            }
+        }
+
+        stage('Sonar Analysis') {
+            // environment {
+            //     scannerHome = tool 'SONAR_SCANNER'
+            // }
+            // steps {
+            //     withSonarQubeEnv('SONAR_INSTALLATION'){
+            //         bat "\"${scannerHome}\\bin\\sonar-scanner\" -e -D\"sonar.projectKey=DeployBac\" -D\"sonar.host.url=http://localhost:9000\" -D\"sonar.login=e88061e20d003a15872df2e7aaa46d97a5be69da\""
+            //     }
+            // }
+            // steps {
+            //     bat 'sonar.projectKey=DeployBac
+            //             sonar.host.url=http://localhost:9000
+            //             sonar.login=e88061e20d003a15872df2e7aaa46d97a5be69da
+            //             sonar.java.binaries=target'
+            // }
+            steps {
+                bat 'mvn clean install'
+            }
+        }
+
+        stage('Quality Gate'){
+            steps {
+                // sleep(5)
+                // timeout(time: 1, unit: 'MINUTES'){
+                //     waitForQualityGate abortPipeline: true
+                // }
+                bat 'mvn test'
+            }
+        }
+
+        stage('Deploy Backend'){
+            steps{
+                deploy adapters: [tomcat9(credentialsId: 'tomcat_login', path: '', url: 'http://localhost:8001/')], contextPath: 'tasks-backend', war: 'target/tasks-backend.war'
+            }
+        }
+
+        stage('API Test'){
+            steps {
+                dir('api-test') {
+                    git credentialsId: 'github_login', url: 'https://github.com/GiuliaNogoliver/tasks-api-test'
+                    bat 'mvn test'
+                }
+            }
+        }
+
+        stage('Deploy Front'){
+            steps {
+                git credentialsId: 'github_login', url: 'https://github.com/GiuliaNogoliver/tasks-frontend'
+                bau 'mvn clean package'
+                deploy adapters: [tomcat9(credentialsId: 'tomcat_login', path: '', url: 'http://localhost:8001/')], contextPath: 'tasks-frontend', war: 'target/tasks-frontend.war'
+            }
+        }
+    }
+}
+
---
+++ src/test/java/br/ce/wcaquino/taskbackend/controller/TaskControllerTest.java
@@ -0,0 +1,82 @@
+package br.ce.wcaquino.taskbackend.controller;
+
+import java.time.LocalDate;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import br.ce.wcaquino.taskbackend.model.Task;
+import br.ce.wcaquino.taskbackend.repo.TaskRepo;
+import br.ce.wcaquino.taskbackend.utils.ValidationException;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(TaskController.class)
+public class TaskControllerTest {
+
+    @MockBean
+    private TaskRepo todoRepo;
+
+    @InjectMocks
+    private TaskController controller;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void naoDeveSalvarTarefaSemDescricao() {
+        Task todo = new Task();
+        todo.setDueDate(LocalDate.now());
+        try {
+            controller.save(todo);
+            Assert.fail("O teste falhou!");
+        } catch (ValidationException e) {
+            Assert.assertEquals("Fill the task description", e.getMessage());
+        }
+    }
+
+    @Test
+    public void naoDeveTarefaSemData() {
+        Task todo = new Task();
+        todo.setTask("Tarefa Teste");
+        try {
+            controller.save(todo);
+            Assert.fail("O teste falhou!");
+        } catch (ValidationException e) {
+            Assert.assertEquals("Fill the due date", e.getMessage());
+        }
+    }
+
+    @Test
+    public void naoDeveSalvarTarefaComDataPassada() {
+        Task todo = new Task();
+        todo.setDueDate(LocalDate.of(2010, 1, 1));
+        todo.setTask("Tarefa Teste");
+        try {
+            controller.save(todo);
+            Assert.fail("O teste falhou!");
+        } catch (ValidationException e) {
+            Assert.assertEquals("Due date must not be in past", e.getMessage());
+        }
+    }
+
+    // @Test
+    public void deveSalvarTerefaComSucesso() throws ValidationException {
+        Task todo = new Task();
+        todo.setDueDate(LocalDate.now());
+        todo.setTask("Tarefa Teste");
+        controller.save(todo);
+        Mockito.verify(todoRepo).save(todo);
+    }
+}
